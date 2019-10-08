41ebb6938789cf6f18c92d8a570c65a7f4056df2
Jan Oster <j.oster@stud.hs-heilbronn.de>
2019-10-08 17:17:57 +0200

    time formatting added
    Signed-off-by: Jan Oster <j.oster@stud.hs-heilbronn.de>

--- exercise_1/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise1/MyAdapter.java
+++ exercise_1/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise1/MyAdapter.java
@@ -11,6 +11,7 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter {
@@ -63,8 +64,12 @@ public class MyAdapter extends RecyclerView.Adapter {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         // holder.textView.setText(mDataset[position]);
-        //TODO hier die Alarme zurück umwandeln
-        ((MyViewHolder) holder).tvDescription.setText(""+ mTimer.get(position).getTime());
+        //TODO schalter aktiv setzten
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(mTimer.get(position).getTime());
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        ((MyViewHolder) holder).tvDescription.setText(hour + " " + minute);
 
         //TODO hier longpress für delete einfügen
     }
@@ -72,7 +77,10 @@ public class MyAdapter extends RecyclerView.Adapter {
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mTimer.size();
+        if (mTimer != null) {
+            return mTimer.size();
+        }
+        return 0;
     }
 
     void setWords(List<Timer> timer) {
