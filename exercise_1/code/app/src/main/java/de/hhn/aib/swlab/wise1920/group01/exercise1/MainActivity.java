03ee500b370a6c6c5665084964c7481b142c8b85
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2019-10-08 15:40:26 +0200

    added SettingsActivity with possibility to change, save and get selected Ringtone using androidx.preferences

--- exercise_1/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise1/MainActivity.java
+++ exercise_1/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise1/MainActivity.java
@@ -1,17 +1,18 @@
 package de.hhn.aib.swlab.wise1920.group01.exercise1;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.preference.PreferenceManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity implements TimePickerFragment.TimePickerListener {
     private TodoRepository todoRepository;
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements TimePickerFragmen
         RecyclerView rvTodos = findViewById(R.id.rvTodos);
         rvTodos.setAdapter(new MyAdapter(todoRepository));
         rvTodos.setLayoutManager(new LinearLayoutManager(this));
-
+        //startActivity(new Intent(this, SettingsActivity.class));
     }
 
     @SuppressLint("SetTextI18n")
@@ -46,6 +47,10 @@ public class MainActivity extends AppCompatActivity implements TimePickerFragmen
         Todo todo = new Todo("Hours = " + hour + " Minutes = " + minute);
         todoRepository.addTodo(todo);
         Log.d("onTimeSet", "time set");
+    }
+
+    public String getRingtonePath(Context context) {
+        return new PreferenceManager(context).getDefaultSharedPreferences(context).getString("ringtone", "bowserlaugh");
     }
 }
 
