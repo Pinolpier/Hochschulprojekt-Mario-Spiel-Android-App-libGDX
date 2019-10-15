d60d3609ed80501b44a843aa9c7893c6ea9cd56c
Chris <chhenri@stud.hs-heilbronn.de>
2019-10-15 22:40:07 +0200

    first commit, simple application that can send current longitude and latitude every 3 seconds when GPS_Service is enabled through button

---
+++ exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/GPS_Service.java
@@ -0,0 +1,71 @@
+package de.hhn.aib.swlab.wise1920.group01.exercise2;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class GPS_Service extends Service
+{
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onCreate() {
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Intent intent = new Intent("location_update");
+                intent.putExtra("coordinates",location.getLongitude()+" "+location.getLatitude());
+                sendBroadcast(intent);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        };
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        //supressed MissingPermission
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3000,0,locationListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(locationManager != null)
+        {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+}
+
--- exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/MainActivity.java
+++ exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/MainActivity.java
@@ -1,14 +1,103 @@
 package de.hhn.aib.swlab.wise1920.group01.exercise2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_start, btn_stop;
+    private TextView coordinates;
+    private BroadcastReceiver broadcastReceiver;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null)
+        {
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    coordinates.append("\n" + intent.getExtras().get("coordinates"));
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(broadcastReceiver != null)
+        {
+            unregisterReceiver(broadcastReceiver);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_stop = findViewById(R.id.btn_stop);
+        coordinates = findViewById(R.id.coordinates);
+
+        if(!check_permissions())
+            enable_buttons();
+
+    }
+
+    private void enable_buttons()
+    {
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GPS_Service.class);
+                startService(intent);
+            }
+        });
+
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GPS_Service.class);
+                stopService(intent);
+            }
+        });
+    }
+
+    private boolean check_permissions()
+    {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                enable_buttons();
+            } else {
+                check_permissions();
+            }
+        }
     }
 }
