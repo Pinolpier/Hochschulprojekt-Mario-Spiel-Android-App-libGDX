9252231f785f814f376f57089147f0b9b3191d96
Jan Oster <joster@stud.hs-heilbronn.de>
2019-10-17 14:27:04 +0200

    added PreferenceManager for AndroidX

--- exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/SettingsActivity.java
+++ exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/SettingsActivity.java
@@ -1,14 +1,39 @@
 package de.hhn.aib.swlab.wise1920.group01.exercise2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    /**
+     * This method is called from a call of the system method startActivity(Intent) only. It starts
+     * the activity and loads the preferences from the preferences file.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
     }
 }
---
+++ exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/SettingsFragment.java
@@ -0,0 +1,5 @@
+package de.hhn.aib.swlab.wise1920.group01.exercise2;
+
+import android.os.Bundle;
+
+
