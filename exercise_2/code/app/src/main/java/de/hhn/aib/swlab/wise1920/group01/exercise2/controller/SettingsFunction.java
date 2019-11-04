9917772796fd0858f3f4dfe5680165eb13417781
Jan Oster <j.oster@stud.hs-heilbronn.de>
2019-11-04 11:27:16 +0100

    merged with settings_joster

--- exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/controller/SettingsFunction.java
+++ exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/controller/SettingsFunction.java
@@ -5,36 +5,36 @@ import android.util.Log;
 public class SettingsFunction {
     public void changeRadius(String s) {
         //TODO chnage radius with use of the string s
-        Log.e("cngRadius", s);
+        Log.wtf("cngRadius", s);
     }
 
     public void changeInterval(String s) {
         //TODO change interval with use of the string s
-        Log.e("cngInterval", s);
+        Log.wtf("cngInterval", s);
     }
 
     public void changePOI(Boolean b) {
         //TODO change showPoi with use of boolean b
-        Log.e("cngPOI", "" + b);
+        Log.wtf("cngPOI", "" + b);
     }
 
     public void changeWeatherdata(Boolean b) {
         //TODO change showWeatherdata with use of boolean b
-        Log.e("cngWatherdata", "" + b);
+        Log.wtf("cngWatherdata", "" + b);
     }
 
     public void changeLocationHistory(Boolean b) {
         //TODO change showLocationhistory with use of boolean b
-        Log.e("cngLocationhistory", "" + b);
+        Log.wtf("cngLocationhistory", "" + b);
     }
 
     public void changeLocationHistoryTimeframe(String s) {
         //TODO change locHistoryTimeframe with use of string s
-        Log.e("cngLocHistorytimeframe", s);
+        Log.wtf("cngLocHistorytimeframe", s);
     }
 
     public void changePassword(String s) {
         //TODO change password with use of string s
-        Log.e("changePassword", s);
+        Log.wtf("changePassword", s);
     }
 }
