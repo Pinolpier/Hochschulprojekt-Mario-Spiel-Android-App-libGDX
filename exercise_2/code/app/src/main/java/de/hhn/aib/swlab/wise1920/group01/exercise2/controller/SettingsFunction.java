ceebc6edde338dff0b99c92db7fe5942d0060c89
Jan Oster <j.oster@stud.hs-heilbronn.de>
2019-11-07 18:18:49 +0100

    Revert "added TODO"
    This reverts commit 2a4d0460

--- exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/controller/SettingsFunction.java
+++ exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/controller/SettingsFunction.java
@@ -2,40 +2,38 @@ package de.hhn.aib.swlab.wise1920.group01.exercise2.controller;
 
 import android.util.Log;
 
-
 public class SettingsFunction {
     public void changeRadius(String s) {
         //TODO chnage radius with use of the string s
-        Log.wtf("cngRadius", s);
+        Log.e("cngRadius", s);
     }
 
     public void changeInterval(String s) {
         //TODO change interval with use of the string s
-        Log.wtf("cngInterval", s);
+        Log.e("cngInterval", s);
     }
 
     public void changePOI(Boolean b) {
         //TODO change showPoi with use of boolean b
-        Log.wtf("cngPOI", "" + b);
+        Log.e("cngPOI", "" + b);
     }
 
     public void changeWeatherdata(Boolean b) {
         //TODO change showWeatherdata with use of boolean b
-        Log.wtf("cngWatherdata", "" + b);
+        Log.e("cngWatherdata", "" + b);
     }
 
     public void changeLocationHistory(Boolean b) {
         //TODO change showLocationhistory with use of boolean b
-        Log.wtf("cngLocationhistory", "" + b);
+        Log.e("cngLocationhistory", "" + b);
     }
 
     public void changeLocationHistoryTimeframe(String s) {
-        //TODO change locHistoryTimeframe with use of string s
-        Log.wtf("cngLocHistorytimeframe", s);
+        Log.e("cngLocHistorytimeframe", s);
     }
 
     public void changePassword(String s) {
         //TODO change password with use of string s
-        Log.wtf("changePassword", s);
+        Log.e("changePassword", s);
     }
 }
