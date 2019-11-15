26b6eb2d4dd6ca7a0c848416a80868abfc7e47a7
Jan Oster <joster@stud.hs-heilbronn.de>
2019-11-15 21:37:21 +0100

    removed settings from loginscreen

--- exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/view/LoginActivity.java
+++ exercise_2/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise2/view/LoginActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -116,19 +114,5 @@ public class LoginActivity extends AppCompatActivity {
                 Log.wtf("LoginActivity: ", "Login onFailure has been called");
             }
         });
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-        }
-        return true;
     }
 }
