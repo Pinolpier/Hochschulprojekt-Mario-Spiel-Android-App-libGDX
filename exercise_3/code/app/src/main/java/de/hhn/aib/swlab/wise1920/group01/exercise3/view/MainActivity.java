ecd3b180522f0e594aef7b63c4c9e60021c806d5
mhaering <mhaering@stud.hs-heilbronn.de>
2019-11-28 19:00:02 +0100

    created simple package structure

--- exercise_3/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise3/view/MainActivity.java
+++ exercise_3/code/app/src/main/java/de/hhn/aib/swlab/wise1920/group01/exercise3/view/MainActivity.java
@@ -1,6 +1,7 @@
-package de.hhn.aib.swlab.wise1920.group01.exercise3;
+package de.hhn.aib.swlab.wise1920.group01.exercise3.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import de.hhn.aib.swlab.wise1920.group01.exercise3.R;
 
 import android.os.Bundle;
 
