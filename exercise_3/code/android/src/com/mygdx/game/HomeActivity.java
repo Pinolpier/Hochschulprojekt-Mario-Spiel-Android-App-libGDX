8ff108a380254c6893969bae6fad872ad431de88
chhenri <chhenri@stud.hs-heilbronn.de>
2019-12-11 19:21:55 +0100

    added buttons and soundmute switch

--- exercise_3/code/android/src/com/mygdx/game/HomeActivity.java
+++ exercise_3/code/android/src/com/mygdx/game/HomeActivity.java
@@ -1,9 +1,9 @@
 package com.mygdx.game;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -16,5 +16,18 @@ public class HomeActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
+    }
+
+    public void exitApp(View v)
+    {
+        finish();
+        System.exit(0);
+    }
+
+    public void startGame(View v)
+    {
+        Intent homeIntent = new Intent(HomeActivity.this, AndroidLauncher.class);
+        startActivity(homeIntent);
+        finish();
     }
 }
