6f675a4fe519794ab475e80c21755764c73f050c
mhaering <mhaering@stud.hs-heilbronn.de>
2019-12-12 03:35:01 +0100

    created SuperMario Solo Game with touch to jump. Movements not working

--- exercise_3/code/android/src/com/mygdx/game/AndroidLauncher.java
+++ exercise_3/code/android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		config.useGLSurfaceView20API18=true;
+		config.useGyroscope=true;
+		config.useCompass=true;
+		initialize(new MarioBros(), config);
 	}
 }
