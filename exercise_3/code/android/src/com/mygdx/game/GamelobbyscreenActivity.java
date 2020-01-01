86dc4d901d6b15ddd2b8287e0617f8fb0df3198b
chhenri <chhenri@stud.hs-heilbronn.de>
2020-01-01 23:37:04 +0100

    Gamelobbyscreenadapter added (Recyclerview)

--- exercise_3/code/android/src/com/mygdx/game/GamelobbyscreenActivity.java
+++ exercise_3/code/android/src/com/mygdx/game/GamelobbyscreenActivity.java
@@ -2,15 +2,35 @@ package com.mygdx.game;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ListView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
 
 public class GamelobbyscreenActivity extends Activity {
-    private ListView listview;
+    private RecyclerView recyclerView;
+    GamelobbyscreenAdapter adapter;
+    ArrayList<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamelobbyscreen);
-        listview = findViewById(R.id.lobbylistView);
+
+        items = new ArrayList<>();
+        items.add("First CardView item");
+        items.add("Second CardView item");
+        items.add("Third CardView item");
+        items.add("Fourth CardView item");
+        items.add("Fifth CardView item");
+        items.add("Sixth CardView item");
+        items.add("Seventh CardView item");
+        items.add("Eigthj CardView item");
+
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new GamelobbyscreenAdapter(this,items);
+        recyclerView.setAdapter(adapter);
     }
 }
